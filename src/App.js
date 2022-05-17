affb4dbb5c29a70981c24b1cfa595acd82805725
Jhonatan C Hurtado <jhonatan.cortes@servinformacion.com>
2022-05-17 09:41:14 -0500

    Develop name

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p> Branch: Main </p>
+        <p> Branch: Develop </p>
       </header>
     </div>
   );
